460ca267c239fe1281a79680436f5b38e0c6a733
Dan Amiri-Fear <danielowenfear@gmail.com>
2018-11-30 03:03:00 +0000

    adds map.js

--- map.js
+++ map.js
@@ -1,46 +1,21 @@
 
+ // EXERCISE MAPPING //
 
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
 
+var result = input.map(inputTwo => {
+  var inputModified = [];
+  inputModified = Math.pow(2, inputTwo.x) + Math.pow(2, inputTwo.y);
+  inputModified = Math.sqrt(inputModified);
+  inputModified = Math.ceil(inputModified);
+  return inputModified;
+})
 
-
-// var input = [
-//   { x: 3, y: 4 },
-//   { x: 12, y: 5 },
-//   { x: 8, y: 15 }
-// ];
-// var result = input.map(x => Math.sqrt(x.x ** 2 + x.y ** 2));
-
-// console.log(result[0] === 5);
-// console.log(result[1] === 13);
-// console.log(result[2] === 17);
-
-
-
-
-
-
-var numbers = [1, 4, 9];
-var result = numbers.map(function(num) {
-  return num * 2;
-  var z = num;
-  console.log(z)
-});
-
+console.log(result[0] === 5);
+console.log(result[1] === 13);
 console.log(result[2] === 17);
 
-
-
-
-
-// function triCalc (coordinate) {
-//   var z = Math.sqrt(coordinate.x * coordinate.x + coordinate.y * coordinate.y)
-//   return z
-// };
-
-// // var coordinate = {x: 3, y: 4};
-// // var z = triCalc(coordinate);
-
-// for (var i = 0; i < input.length; i++) {
-//   var coordinate = input[i];
-//   console.log(triCalc(coordinate))
-// };
