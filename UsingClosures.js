49618c317dc5deed12054b3061cc5f4ecc722aaa
Dan Amiri-Fear <danielowenfear@gmail.com>
2018-11-30 01:23:22 +0000

    adds UsingClosures.js First Ex

---
+++ UsingClosures.js
@@ -0,0 +1,31 @@
+
+
+// EXERCISE ONE //
+
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var dice;
+  var i = 0;
+
+return function() {
+  dice = list[i];
+  i++;
+  return dice;
+
+return die
+
+  }
+
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
