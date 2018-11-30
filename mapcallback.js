7a0a7dc0fdee9ce1d99f26b54d29cfd964598998
Dan Amiri-Fear <danielowenfear@gmail.com>
2018-11-30 00:24:38 +0000

    adds mapcallback

---
+++ mapcallback.js
@@ -0,0 +1,29 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+function map(array, callback) {
+  var arrayMod = [];
+  for (var i = 0; i < array.length; i++){
+    // console.log(array);
+    arrayMod.push(callback(array[i]));
+  }
+    console.log(arrayMod);
+}
+// console.log(arrayMod);
+
+
+
+
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+
